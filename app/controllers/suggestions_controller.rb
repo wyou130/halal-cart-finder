70fe92efe93845621de27ade0fa23b1a3aa60bd2
Wendy You <wyou130@gmail.com>
2023-01-13 14:35:49 -0500

    Added actions in suggestions controller, tested with Postman, and added error handling.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,2 +1,33 @@
 class SuggestionsController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid
+
+    def index
+        render json: Suggestion.all, status: :ok
+    end
+
+    def show
+        render json: Suggestion.find(params[:id]), status: :ok
+    end
+
+    def create
+        render json: Suggestion.create!(sugg_params), status: :created
+    end
+
+    def destroy
+        render json: Suggestion.find(params[:id]).destroy!
+        head :no_content
+    end
+
+    private
+
+    def sugg_params
+        params.permit(:name, :street, :avenue, :landmarks, :accepts_card, :opening_hours, :closing_hours, :opening_am_pm, :closing_am_pm, :chicken_over_rice, :combo_over_rice, :latitude, :longitude, :image, :user_id)
+    end
+
+    def not_found
+        render json: { error: 'Suggestion not found' }, status: :not_found
+    end
+
 end
